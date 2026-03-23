98ff154c57d51102f34fc466031cbe85d9e82136
patte <patternlouis@gmail.com>
2026-03-23 18:37:00 +0000

    Tea project

--- main.py
+++ main.py
@@ -8,23 +8,25 @@ import scipy.constants as const
 from math import pi
 
 
-# function for the ODE
+# function for the differential equation
+# this, and the parameters in it can be modified
 def f(t, T):
     # assume parameters
     sigma = const.sigma
-    T_room = 289  # room temperature (K)
+    T_room = 292.15  # room temperature (K)
     k_mug = 1.5  # thermal conductivity of the mug material (W/m/K)
-    r = 0.04 # radius of the mug (m)
+    r = 0.045 # radius of the mug (m)
+    height = 0.06
     area = pi*r**2
-    area_surf = 2*pi*r*0.1 + area
+    area_surf = (2*pi*r*height) + (2*area)
     rho = 1000
-    m = rho * area * 0.05
+    m = rho * area * height
     cp = 4183  # specific heat capacity of the liquid (J/kg/K)
     h = 10  # convective heat transfer coefficient (W/m^2K)
-    d = 0.005  # thickness of the mug wall (m)
-    h_eff = 1/((d/k_mug) + (1/h))  # effective heat transfer coefficient considering conduction through the mug wall
+    d = 0.002  # thickness of the mug wall (m)
     em = 0.9  # emissivity of the mug surface
 
+    h_eff = 1 / ((d / k_mug) + (1 / h))  # effective heat transfer coefficient
     Q_dot_rad = em*sigma*area_surf*((T**4) - (T_room**4))
     Q_dot_cond_conv = h_eff*area_surf*(T - T_room)
 
@@ -33,6 +35,7 @@ def f(t, T):
     return dT_dt
 
 
+# runge-kutta (RK4) method
 def runge_kutta(t0, y0, t_end, h):
     t_values = np.arange(t0, t_end + h, h)
     y_values = np.zeros(len(t_values))
@@ -67,9 +70,10 @@ if __name__ == "__main__":
     # set parameters
     t0 = 0 # minutes
     # starting temperature (K)
-    y0 = 364.15
+    #y0 = 364.15
+    y0 = 356.15
     # how long to run the simulation (s)
-    t_end = 1200
+    t_end = 7200
     # step size
     h = 1
 
@@ -77,17 +81,18 @@ if __name__ == "__main__":
     t_values, y_values = runge_kutta(t0, y0, t_end, h)
     y_values -= 273.15  # convert to Celsius for plotting
     # Euler method
-    # t_values_euler, y_values_euler = euler(t0, y0, t_end, h)
+    #t_values_euler, y_values_euler = euler(t0, y0, t_end, h)
+    #y_values_euler -= 273.15
 
     # Plotting the results
     plt.figure(figsize=(10, 6))
     plt.plot(t_values, y_values,"-", label='Runge-Kutta (just water)', color='blue')
-    # plt.plot(t_values_euler, y_values_euler,".", label='Euler', color='red')
+    #plt.plot(t_values_euler/60, y_values_euler,".", label='Euler', color='red')
 
-    plt.plot(t_end, 55, "x", label='55', color='green')
+    plt.plot(t_end, 19, "x", label='19 (room temp)', color='green')
 
     plt.title("Modelled temperature of tea against time")
-    plt.xlabel("Time (s)")
+    plt.xlabel("Time (minutes)")
     plt.ylabel("Temperature (deg C)")
     plt.legend()
     plt.grid()
